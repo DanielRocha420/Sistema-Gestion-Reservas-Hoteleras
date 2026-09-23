947d939021b7007b2217eb79ffaf13ce2d483719
NayeHernandezS <nayelyhernandezsilva@MacBook-Air-de-Nayely.local>
2026-09-23 17:04:14 -0600

    subiendo solo huespedes 2 da parte

---
+++ commons/src/main/java/com/daniel/commons/client/HuespedClient.java
@@ -0,0 +1,4 @@
+package com.daniel.commons.client;
+
+public class HuespedClient {
+}
--- huespedes/src/main/java/com/reservashoteleras/huespedes/repository/HuespedesRepository.java
+++ huespedes/src/main/java/com/reservashoteleras/huespedes/repository/HuespedesRepository.java
@@ -1,7 +1,11 @@
 package com.reservashoteleras.huespedes.repository;
 
+import com.daniel.commons.enums.EstadoRegistro;
 import com.reservashoteleras.huespedes.entity.Huesped;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface HuespedesRepository extends JpaRepository<Huesped, Long> {
 
@@ -16,4 +20,8 @@ public interface HuespedesRepository extends JpaRepository<Huesped, Long> {
     boolean existsByTelefonoAndIdNot(String telefono, Long id);
 
     boolean existsByDocumentoAndIdNot(String documento, Long id);
+
+    List<Huesped> findByEstado(EstadoRegistro estado);
+
+
 }
