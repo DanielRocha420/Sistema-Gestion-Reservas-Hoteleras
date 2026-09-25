a7888cff90819cc6e2bffaca5696a92b7a59fea7
DanielRocha420 <danielhernadezrocha9@gmail.com>
2026-09-24 18:30:08 -0600

    Eliminar reservasApp del repositorio (se conserva localmente)

--- reservasApp/src/environments/enviroments.prod.ts
+++
@@ -1,5 +0,0 @@
-export const enviroments = {
-    production: true,
-    authUrl: 'http://localhost:9000',
-    apiUrl: 'http://localhost:8090'
-};
--- reservasApp/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  authUrl: 'http://localhost:9000',
-  huespedesUrl: 'http://localhost:8081',
-  habitacionesUrl: 'http://localhost:8082',
-  reservasUrl: 'http://localhost:8083'
-};
